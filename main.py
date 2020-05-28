a8a0d4d1f3f40f1fe9812e450bd20e8dbfb7a828
William Otieno <jimmywilliamotieno@gmail.com>
2020-05-28 23:49:25 +0300

    Commit

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import time
+from secrets import password
 
 
 
 
 class Instabot:
-    def __init__(self):
+    def __init__(self, username, password):
 
         options = Options()
         options.add_experimental_option("detach", True)
         self.driver = webdriver.Chrome(options=options)
         self.driver.get("https://instagram.com")
+        time.sleep(4)
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input").send_keys(username)
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input").send_keys(password)
+        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]/button/div").click()
+        self.driver.find_element_by_xpath("/html/body/div[4]/div/div/div[3]/button[2]").click()
 
 
-Instabot()
+Instabot('_ch.ills', password)
---
+++ secrets.py
@@ -0,0 +1,1 @@
+password = 'shadowwalker101'
