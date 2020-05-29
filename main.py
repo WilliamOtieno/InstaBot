67cadaa3973039e9b1b1cb51ff181b833f07542f
William Otieno <jimmywilliamotieno@gmail.com>
2020-05-29 08:02:35 +0300

    Finalized login

--- main.py
+++ main.py
@@ -4,20 +4,23 @@ import time
 from secrets import password
 
 
-
-
 class Instabot:
     def __init__(self, username, password):
-
+        self.username = username
+        self.password = password
         options = Options()
         options.add_experimental_option("detach", True)
         self.driver = webdriver.Chrome(options=options)
         self.driver.get("https://instagram.com")
         time.sleep(4)
-        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input").send_keys(username)
-        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input").send_keys(password)
-        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]/button/div").click()
+        self.driver.find_element_by_xpath(
+            "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input").send_keys(username)
+        self.driver.find_element_by_xpath(
+            "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input").send_keys(password)
+        self.driver.find_element_by_xpath(
+            "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]/button/div").click()
+        time.sleep(5)
         self.driver.find_element_by_xpath("/html/body/div[4]/div/div/div[3]/button[2]").click()
 
 
-Instabot('_ch.ills', password)
+my_bot = Instabot('_ch.ills', password)
